bd7f3fef9d8f6edeb46f011b30938a5b3a2d5f08
Azer Sioud <asioud@3-G-8.42heilbronn.de>
2023-04-27 14:28:48 +0200

    cpp 00 ex 00 fixed

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -3,32 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azer <sioudazer8@gmail.com>                +#+  +:+       +#+        */
+/*   By: asioud <asioud@42heilbronn.de>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 06:45:36 by azer              #+#    #+#             */
-/*   Updated: 2022/12/23 20:11:36 by azer             ###   ########.fr       */
+/*   Updated: 2023/04/27 14:27:15 by asioud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <vector>
 #include <ctype.h>
 
-using namespace std;
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	if (argc < 2)
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" <<std::endl;
+	}
 	else
 	{
-		vector<string> arguments (argv, argv+argc); // arguments range from argv[0] to argv[argc]
-		for (int i = 1; i < argc; i++)
-			for (int j = 0; arguments[i][j]; j++)
-				cout << (char) toupper(arguments[i][j]);
-
-		cout << endl;
+		for (int i = 1; i < argc; ++i)
+		{
+			std::string input(argv[i]);
+			for (size_t j = 0; j < input.size(); j++)
+			{
+				input[j] = std::toupper(input[j]);
+			}
+				
+			std::cout << input;
+		}
+		std::cout << std::endl;
 	}
-
-	return 1;
+	return 0;
 }
+
